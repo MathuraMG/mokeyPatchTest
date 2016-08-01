9d8070d2f9950b8f218393b061e9179e5fc23f60
MathuraMG <mmg9291@gmail.com>
2016-08-01 13:31:08 -0400

    add stacktrace

---
+++ Ideas.md
@@ -0,0 +1,5 @@
+### How to reduce verbosity in draw?
+
+* how will the decorator know when to be called ?
+* WHAT is considered a difference and what is not ?
+* can the decorator be smart enough to check the content and repeat or not repeat ?
--- intercept-p5.js
+++ intercept-p5.js
@@ -1,4 +1,32 @@
+console.log('a test comment');
+var inDraw = false;
+
 var shadowDOMElement;
+// var syntax = esprima.parse('var answer = 42');
+
+var callback = function(stackframes) {
+  var stringifiedStack = stackframes.map(function(sf) {
+    // console.log(sf.functionName);
+    return sf.toString();
+  }).join('\n');
+  console.log(stringifiedStack);
+};
+var isInDraw = function(stackframes) {
+  // console.log(stackframes);
+  stringifiedStack = false;
+  stackframes.forEach(function(sf) {
+    // console.log(sf.functionName);
+    if(sf.functionName.localeCompare('draw') === 0) {
+      inDraw = true;
+    } else {
+      // inDraw = false;
+    }
+  })
+  return inDraw;
+};
+var errback = function(err) { console.log(err.message); };
+
+
 
 const BLACKLIST = [
   'createCanvas',
@@ -6,7 +34,10 @@ const BLACKLIST = [
 ];
 
 function createShadowDOMElement() {
+
   var c = document.getElementsByTagName('canvas')[0];
+  c.setAttribute("tabIndex","0");
+  c.setAttribute("role","region");
   var p = document.createElement('p');
   p.id = "shadowDOM-content";
   c.appendChild(p);
@@ -30,20 +61,36 @@ funcNames = funcNames.filter(function(x) {
 // debugger;
 
 funcNames.forEach(function(x){
+
   var originalFunc = p5.prototype[x.name];
 
   p5.prototype[x.name] = function(){
     originalFunc.apply(this,arguments);
+    // console.log( arguments);
+    orgArg = arguments;
+    // StackTrace.get().then(isInDraw).catch(errback)
     if(!shadowDOMElement){
       createShadowDOMElement();
     }
     var content= '';
-    for(var j =0;j<arguments.length;j++) {
-      content = content.concat(x.params[j].description + ' ' + arguments[j] + ' \n');
+    console.log( orgArg);
+    for(var j =0;j<orgArg.length;j++) {
+      console.log('test == ' + x.params[j].description + ' ' + orgArg[j] + ' -- ');
+      content = content.concat(x.params[j].description + ' ' + orgArg[j] + ' -- ');
     }
+    var tempContent = document.getElementById('shadowDOM-content').innerHTML;
+    tempContent = tempContent.split('\n');
     content = content.concat(' \n');
     shadowDOMElement.innerHTML = shadowDOMElement.innerHTML.concat(content);
-  }
 
+  //TODO: Implement inside this
+  var temp = StackTrace.get().then(isInDraw).then(function() {
+    if(inDraw) {
+      //TODO: Figure out what output goes in here - needs to be a bit different (??)
+      // console.log('I dont know what I am doing here!!!');
+    } else {
+    }
+  });
 
+}
 });
